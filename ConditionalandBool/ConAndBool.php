8f98a4a9bb635c5b20705ab8baecf79d3b2b13b1
akil <ak1lt4hsin@gmail.com>
2024-08-28 18:25:07 +0600

    Silly Practices

---
+++ ConditionalandBool/ConAndBool.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Condition & Bool</title>
+</head>
+<body>
+  <h1>Testing for conditional rendering</h1>
+
+  <?php
+  $isMale = true;
+
+  if($isMale != true){
+    echo "You are not a male"; 
+  }
+  else{
+    echo "You are male";
+  }
+  ?>
+
+  
+</body>
+</html>
